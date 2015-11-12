436acb687b189cfe7e455ff3e0ad448d58b833b1
Oleksandr Natalenko <o.natalenko@lanet.ua>
2015-11-12 10:46:13 +0200

    move dentry types to separate namespace

--- pfcfsq.h
+++ pfcfsq.h
@@ -20,13 +20,13 @@
 #ifndef __PFCFSQ_H__
 #define __PFCFSQ_H__
 
-typedef void (*dentry_handler_t)(struct ceph_mount_info*, const char*, struct dirent*, struct stat*, void*, unsigned int);
-typedef int (*dentry_comparator_t)(const char*, struct dirent*, struct stat*, unsigned int);
+typedef void (*cfs_dentry_handler_t)(struct ceph_mount_info*, const char*, struct dirent*, struct stat*, void*, unsigned int);
+typedef int (*cfs_dentry_comparator_t)(const char*, struct dirent*, struct stat*, unsigned int);
 
 struct ceph_mount_info* cfs_mount(const char* _mons, const char* _id, const char* _keyring_file, const char* _root) __attribute__((nonnull(1, 2, 3, 4)));
 int cfs_unmount(struct ceph_mount_info* _fs) __attribute__((nonnull(1)));
 int cfs_mkdir_safe(struct ceph_mount_info* _fs, const char* _path, mode_t _mode);
-void walk_dir_generic(struct ceph_mount_info* _fs, const char* _entry_point, dentry_handler_t _handler, dentry_comparator_t _comparator, void* _data, unsigned int _level) __attribute__((nonnull(1, 2, 3)));
+void walk_dir_generic(struct ceph_mount_info* _fs, const char* _entry_point, cfs_dentry_handler_t _handler, cfs_dentry_comparator_t _comparator, void* _data, unsigned int _level) __attribute__((nonnull(1, 2, 3)));
 
 #endif /* __PFGFQ_H__ */
 
