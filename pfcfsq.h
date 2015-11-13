6f879b75acbaa62f2ed90a5cceeb3a3c717eb7eb
Oleksandr Natalenko <o.natalenko@lanet.ua>
2015-11-13 10:24:20 +0200

    fix comment

--- pfcfsq.h
+++ pfcfsq.h
@@ -28,5 +28,5 @@ int cfs_unmount(struct ceph_mount_info* _fs) __attribute__((nonnull(1)));
 int cfs_mkdir_safe(struct ceph_mount_info* _fs, const char* _path, mode_t _mode);
 void cfs_walk_dir_generic(struct ceph_mount_info* _fs, const char* _entry_point, cfs_dentry_handler_t _handler, cfs_dentry_comparator_t _comparator, void* _data, unsigned int _level) __attribute__((nonnull(1, 2, 3)));
 
-#endif /* __PFGFQ_H__ */
+#endif /* __PFCFSQ_H__ */
 
